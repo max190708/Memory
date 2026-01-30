94a8b3308f446dc67fe8fd2b2c826e215458350f
max190708 <yelisieiev.maksym.studente@itispaleocapa.it>
2026-01-30 11:49:23 +0100

    Add files via upload
    Generazione della tabella e delle lettere

---
+++ memory.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <ctime>
+#include <stdlib.h>
+using namespace std;
+// funzione
+	void generazione (int &a, int &b)
+	{
+	a=rand()%12+4;
+	b=rand()%12+4;
+	}
+int main(int argc, char** argv) {
+	srand(time(0));
+	int x,y;
+	generazione (x,y);
+	do
+	{
+		generazione (x,y);
+	}while (x*y%2!=0 || x*y>52);
+	int c[x][y]={0}; //la matrice
+	for(int i=0;i<x;i++)
+	{
+		cout<<endl<<" * ";
+		for(int r=1;r<y;r++)
+		{
+			cout<<" * ";
+		}
+	}
+	int b=x*y/2;
+	char a [b]={};
+	for(int i=0;i<b;i++)
+	{
+		a[i]=rand()%26+65;
+		cout<<"Il "<<i+1<<" valore è: "<<a<<endl;
+	}
+	
+	return 0;
+}
